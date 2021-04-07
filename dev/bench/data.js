0b0151b8a6d55af7882fe36b4c44330a4e4c35c9
github-action-benchmark <github@users.noreply.github.com>
2021-04-07 05:08:22 +0000

    add pipeline (cargo) benchmark result for aa4a2eaa44ce4eb8a0049e4363f59f323ed56854

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,6 +1,6 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1616785700335,
-  "repoUrl": "https://github.com/hyperium/hyper",
+  "lastUpdate": 1617772102522,
+  "repoUrl": "https://github.com/kevinburke/hyper",
   "entries": {
     "connect": [
       {
@@ -4111,6 +4111,36 @@ window.BENCHMARK_DATA = {
             "name": "hello_world_16",
             "value": 60428,
             "range": "± 15684",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "kevin@burke.dev",
+            "name": "Kevin Burke",
+            "username": "kevinburke"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "aa4a2eaa44ce4eb8a0049e4363f59f323ed56854",
+          "message": "chore(LICENSE): update license year (#2491)",
+          "timestamp": "2021-04-06T14:49:37-07:00",
+          "tree_id": "b6f6db86e5b8ff78bec3e88897081599827b72bd",
+          "url": "https://github.com/kevinburke/hyper/commit/aa4a2eaa44ce4eb8a0049e4363f59f323ed56854"
+        },
+        "date": 1617772100868,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "hello_world_16",
+            "value": 46548,
+            "range": "± 2049",
             "unit": "ns/iter"
           }
         ]
